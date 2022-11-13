885b05746c6b412688b466555fd3a7327c480440
nurjamil zulianto afsyari <nurjamilzulianto.a@gmail.com>
2022-11-13 18:09:43 +0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Nurjamil Zulianto Afsyari on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
